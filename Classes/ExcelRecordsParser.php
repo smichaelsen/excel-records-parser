3db019a4c907808d162e13cbd6bc889888ce2ac7
Sebastian Michaelsen <sebastian@michaelsen.io>
2017-01-25 15:48:17 +0100

    [FEATURE] map values

--- Classes/ExcelRecordsParser.php
+++ Classes/ExcelRecordsParser.php
@@ -19,19 +19,9 @@ class ExcelRecordsParser
                 }
                 $record = [];
                 foreach ($parsingConfiguration['fields'] as $fieldName => $fieldParsingConfiguration) {
-                    $possibleColumnNames = array_map('trim', explode('//', $fieldParsingConfiguration['column']));
-                    $value = null;
-                    $possibleColumnName = null;
-                    foreach ($possibleColumnNames as $possibleColumnName) {
-                        $cellName = $possibleColumnName . $row->getRowIndex();
-                        $value = $worksheet->getCell($cellName)->getValue();
-                        if (!empty($value)) {
-                            break;
-                        }
-                    }
-                    if (is_callable($fieldParsingConfiguration['transform'])) {
-                        $value = call_user_func($fieldParsingConfiguration['transform'], $value);
-                    }
+                    $value = $this->getValue($worksheet, $row, $fieldParsingConfiguration);
+                    $value = $this->mapValue($value, $fieldParsingConfiguration);
+                    $value = $this->transformValue($value, $fieldParsingConfiguration);
                     if ($value !== null) {
                         $record[$fieldName] = $value;
                     }
@@ -39,6 +29,44 @@ class ExcelRecordsParser
                 yield $record;
             }
         }
+    }
+
+    protected function getValue(\PHPExcel_Worksheet $worksheet, \PHPExcel_Worksheet_Row $row, array $fieldParsingConfiguration): string
+    {
+        $possibleColumnNames = array_map('trim', explode('//', $fieldParsingConfiguration['column']));
+        $possibleColumnName = null;
+        foreach ($possibleColumnNames as $possibleColumnName) {
+            $cellName = $possibleColumnName . $row->getRowIndex();
+            $value = $worksheet->getCell($cellName)->getValue();
+            if (!empty($value)) {
+                return $value;
+            }
+        }
+        return '';
+    }
+
+    protected function mapValue(string $value, array $fieldParsingConfiguration): string
+    {
+        if (!isset($fieldParsingConfiguration['map'])) {
+            return $value;
+        }
+        if ($fieldParsingConfiguration['map_mode'] === 'strict' && !in_array($value, array_keys($fieldParsingConfiguration['map']))) {
+            throw new \Exception('Value "' . $value . '" could not be mapped. Strict mode."', 1485343840);
+        }
+        foreach ($fieldParsingConfiguration['map'] as $match => $replace) {
+            if ($value === $match) {
+                return $replace;
+            }
+        }
+        return $value;
+    }
+
+    protected function transformValue(string $value, array $fieldParsingConfiguration): string
+    {
+        if (!is_callable($fieldParsingConfiguration['transform'])) {
+            return $value;
+        }
+        return (string)call_user_func($fieldParsingConfiguration['transform'], $value);
     }
 
 }
